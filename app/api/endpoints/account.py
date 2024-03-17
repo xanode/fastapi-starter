a00cc36f0d14259451ef998113c769e2ad5f1b8b
Benoit MARZELLEAU <benoit.marzelleau@telecom-sudparis.eu>
2024-03-17 02:40:52 +0100

    Use `gettext` based i18n in `account.py`

--- app/api/endpoints/account.py
+++ app/api/endpoints/account.py
@@ -2,7 +2,6 @@ import logging
 
 from fastapi import APIRouter, Depends, HTTPException, Security, status
 
-from app.core.translation import Translator
 from app.core.types import SecurityScopes
 from app.core.utils.misc import process_query_parameters, to_query_parameters
 from app.crud.crud_account import account as accounts
@@ -10,7 +9,6 @@ from app.dependencies import DBDependency, get_current_active_account
 from app.schemas import account as account_schema
 
 router = APIRouter(tags=["account"], prefix="/account")
-translator = Translator()
 
 logger = logging.getLogger("app.api.account")
 
@@ -47,7 +45,7 @@ async def create_account(account: account_schema.AccountCreate, db: DBDependency
         logger.debug(f"Username {account.username} already exists")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=translator.USERNAME_UNAVAILABLE,
+            detail=_("UNAVAILABLE_USERNAME"),
         )
     return await accounts.create(db, obj_in=account)
 
@@ -66,7 +64,7 @@ async def read_account(account_id: int, db: DBDependency):
     account = await accounts.read(db, account_id)
     if account is None:
         logger.debug(f"Account {account_id} not found")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=translator.ELEMENT_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=_("ELEMENT_NOT_FOUND"))
     return account
 
 
@@ -84,13 +82,13 @@ async def update_account(account_id: int, account: account_schema.AccountUpdate,
     old_account = await accounts.read(db, account_id)
     if old_account is None:
         logger.debug(f"Account {account_id} not found")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=translator.ELEMENT_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=_("ELEMENT_NOT_FOUND"))
     results = await accounts.query(db, username=account.username)
     if results and results[0].id != old_account.id:
         logger.debug(f"Username {account.username} already exists")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=translator.USERNAME_UNAVAILABLE,
+            detail=_("UNAVAILABLE_USERNAME"),
         )
     return await accounts.update(db, db_obj=old_account, obj_in=account)
 
@@ -109,5 +107,5 @@ async def delete_account(account_id: int, db: DBDependency):
     account = await accounts.read(db, account_id)
     if account is None:
         logger.debug(f"Account {account_id} not found")
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=translator.ELEMENT_NOT_FOUND)
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=_("ELEMENT_NOT_FOUND"))
     return await accounts.delete(db, id=account_id)
